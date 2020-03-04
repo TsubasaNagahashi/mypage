26f086bcad1f3070344b8ff5e8bf37182bba6873
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2020-03-04 19:15:18 +0900

    finish view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/career',    to: 'static_pages#career'
   get  '/portfolio', to: 'static_pages#portfolio'
+  get  '/skill', to: 'static_pages#skill'
 end
