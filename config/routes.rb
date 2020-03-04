e5493ac594393a6cfba53ae35687761c978a3567
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2020-03-04 13:55:16 +0900

    finished screen transition

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get  '/career',    to: 'static_pages#career'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/profile', to: 'static_pages#profile'
+  get  '/portfolio', to: 'static_pages#portfolio'
 end
