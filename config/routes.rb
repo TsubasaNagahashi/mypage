9a6499d884d857df2579587a8c04e9015c42224b
TsubasaNagahashi <n_t190527@yahoo.co.jp>
2020-03-04 15:10:32 +0900

    view fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/career',    to: 'static_pages#career'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/profile', to: 'static_pages#profile'
   get  '/portfolio', to: 'static_pages#portfolio'
 end
